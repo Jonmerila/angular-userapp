defdb108b29a5dda87f9f03e4bb6a060c9def3a9
Jon Merilä <jon.merila97@hotmail.com>
2024-11-18 13:42:36 +0100

    Created single Task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,20 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
 
-  selectedUserId = "u1";
+  selectedUserId?: string;
   
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,25 @@
 import { Component, Input } from "@angular/core";
+import { TaskComponent } from "./task/task.component";
 
 
 
 @Component({
     selector: 'app-tasks',
     standalone: true,
-    imports: [],
+    imports: [TaskComponent],
     templateUrl: './tasks.component.html',
     styleUrl: './tasks.component.css'
 })
 
 export class TasksComponent{
     @Input({required: true}) name?: string;
+    tasks = [
+        {
+            id: "t1",
+            userId: "u1",
+            title: "Master Angular",
+            summary: "Learn to become good enough on Angular",
+            dueDate: "2025-12-31",
+        }
+    ]
 }
